041b16656b0e3c54ef695af698d5240f8f8eaedb
jeldahsalsabilah08 <147676414+jeldahsalsabilah08@users.noreply.github.com>
2025-11-08 19:18:17 +0700

    Add files via upload

---
+++ meet 3/meet3.py
@@ -0,0 +1,18 @@
+# will will lear about data type
+
+x = 10 # bilangan bulat (integer)
+print("nilai dari x =", x)
+print("tipe data dari x =", type(x))
+
+y = 12.5 # bilangan pecahan (float)
+print("nilai dari y =", y)
+print("tipe data dari y =", type(y))
+
+ukuran = "xxl" # tipe data (string)
+print("nilai dari ukuran =", ukuran)
+print("tipe data dari ukuran =", type(ukuran))
+
+kebenaran = True # tipe data boolean (true or false)
+print("nilai dari kebenaran =", kebenaran)
+print("tipe data dari kebenaran =", type(kebenaran))
+
