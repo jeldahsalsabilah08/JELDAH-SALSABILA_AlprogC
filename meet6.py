6c6f2ea4babacfa111046b2a8f40c0f0cbfb420d
jeldahsalsabilah08 <147676414+jeldahsalsabilah08@users.noreply.github.com>
2025-12-06 12:52:53 +0800

    Add files via upload

---
+++ meet6.py
@@ -0,0 +1,20 @@
+# latihan aritmatika, aplikasi
+# buatlah program konversi suhu dengan output sebagai berikut
+#nama :
+#nim  :
+# masukan suhu anda dalam celsius 
+# suhu anda dalam fahrenheit:
+# suhu anda dalam reamur:
+# suhu anda dalam celvin
+
+# jawab :
+print("nama : Jeldah Talia Salsabilah")
+print("nim  : 23241079 \n")
+
+celsius = float(input("Masukan suhu anda dalam celsius : "))
+fahren = (celsius * 9/5) + 32
+print("Suhu anda dalam fahrenheit :", fahren)
+reamur = celsius * 4/5
+print("Suhu anda dalam reamur     :", reamur)
+kelvin = celsius + 273.15
+print("Suhu anda dalam kelvin     :", kelvin)
